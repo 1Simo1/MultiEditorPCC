53217ccd55aabdb5a890369b35b5c928d3336d99
Simone <simone.lai@gmail.com>
2025-06-21 23:18:54 +0200

    TestoCP

--- MultiEditorPCC/ViewModels/MainViewModel.cs
+++ MultiEditorPCC/ViewModels/MainViewModel.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using MvvmGen;
 using MvvmGen.Events;
 using MvvmGen.ViewModels;
+using System;
+using System.Text;
 using static MultiEditorPCC.EventiMVVM;
 
 namespace MultiEditorPCC.ViewModels;
@@ -22,8 +23,21 @@ public partial class MainViewModel : IEventSubscriber<ConfermatoNuovoProgettoAtt
 
     [Property] private String _versionePCC;
 
+    [Property] private String _testoCP;
+
+    private readonly String a = $@"MjAyNSBTaW1vbmUgcGV
+                                   yIFBDQ2FsY2lvNEV2ZXIgaHR0
+                                   cHM6Ly9wY2NhbGNpbzRldmVyLmZ
+                                   vcnVtY29tbXVuaXR5Lm5ldC8gUHJvZ2V0dG8g
+                                   T3BlbiBTb3VyY2UgOiBodHRwczovL2dpdGh1Yi5
+                                   jb20vMVNpbW8xL011bHRpRWRpdG9yUENDVmVyc2lvbm
+                                   UgMC4xIGFscGhhIChpbiBjb3N0cnV6aW9uZSk =";
+
     partial void OnInitialize()
     {
+        TestoCP = Encoding.ASCII.GetString(Convert.FromBase64String(a)).Replace("PCCV", "PCC V");
+
+
         if (Pag == null)
         {
 
