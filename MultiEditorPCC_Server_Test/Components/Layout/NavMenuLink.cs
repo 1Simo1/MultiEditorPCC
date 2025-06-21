04d90afd29460f11bcbf560fb4116adc13980cf2
Simone <37436823+1Simo1@users.noreply.github.com>
2025-06-21 22:29:08 +0200

    Delete directory

--- MultiEditorPCC_Server_Test/Components/Layout/NavMenuLink.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MultiEditorPCC.Components.Layout;
-
-internal class NavMenuLink
-{
-    public int Id { get; set; } = 0;
-    public String Link { get; set; } = "/";
-    public String IconString { get; set; } = "fa-solid fa-notdef";
-    public String Title { get; set; } = "LinkText";
-}
