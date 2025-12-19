465516564abaa53bf2b261438be0030125167e39
Simone <37436823+1Simo1@users.noreply.github.com>
2025-12-19 12:51:14 +0100

    Aggiunta versione archivi DB

--- MultiEditorPCC.Lib/VersionePCCSupportataEditor.cs
+++ MultiEditorPCC.Lib/VersionePCCSupportataEditor.cs
@@ -6,4 +6,6 @@ public class VersionePCCSupportataEditor
     public String NomeGioco { get; set; }
     public List<String> CercaFilePattern { get; set; }
     public List<String> CartelleSuperiori { get; set; }
+
+    public int VersioneArchiviDB { get; set; } = -1;
 }
